0b5a8fd75bb795d842f195d88f69862e7f1eb6bf
OvyFrite <rakotozananyandy00@gmail.com>
2025-10-28 16:01:41 +0300

    data

---
+++ include/data.php
@@ -0,0 +1,6 @@
+<?php
+$data = array(
+    array('Nom' => 'Alice', 'Email' => 'alice@gmail.com', 'pwd' => '1'),
+    array('Nom' => 'Bob', 'Email' => 'bob@gmail.com', 'pwd' => '1'),
+    array('Nom' => 'Charlie', 'Email' => 'charlie@gmail.com', 'pwd' => '1')
+);
